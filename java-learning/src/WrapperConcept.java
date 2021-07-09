f8bb6a87c3d5d276a7cb467c174ca47e2d60ce8a
Samruddhi-Badlani <samruddhi4227132@gmail.com>
2021-07-10 00:56:14 +0530

    Learned some more methods of wrapper classes

--- java-learning/src/WrapperConcept.java
+++ java-learning/src/WrapperConcept.java
@@ -5,6 +5,13 @@ public class WrapperConcept {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		WrapperConcept myObjectC= new WrapperConcept();
+		String string = "245";
+		int number = Integer.parseInt(string);
+		double floating = Double.parseDouble(string);
+		System.out.println(number);
+		System.out.println(floating);
+		string = Integer.toString(number);
+		System.out.println(string);
 		myObjectC.go();
 	}
 	public void  go() {
