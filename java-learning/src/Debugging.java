b9980115976c5659ccf06cf46a135b6c1e2c5e35
Samruddhi-Badlani <samruddhi4227132@gmail.com>
2021-07-11 15:19:17 +0530

    Learned debugging

---
+++ java-learning/src/Debugging.java
@@ -0,0 +1,16 @@
+
+public class Debugging {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		foo(9);
+		int number=0;
+		System.out.print(number);
+	}
+	public static void foo(int marks) {
+		int my_marks=marks;
+		System.out.print("FOo");
+	}
+
+}
