497625e0e7fbbb339bab9a10027240459c327780
Samruddhi-Badlani <samruddhi4227132@gmail.com>
2021-07-19 15:10:28 +0530

    String class learning

---
+++ java-learning/src/My_String.java
@@ -0,0 +1,12 @@
+
+public class My_String {
+
+	public static void main(String[] args) {
+		String myString = "Hello";
+		myString="hello world";
+		System.out.print(myString+"\n");
+		System.out.println(myString.charAt(1));
+		System.out.println(myString.substring(6));
+		System.out.println(myString.length());
+	}
+}
